fc7ca48bce26f74bee636ce8349aa3861ad1e1a7
Andrey Tykhonov <atykhonov@gmail.com>
2014-01-23 22:13:03 +0200

    Ecukes has been initiated

---
+++ features/support/env.el
@@ -0,0 +1,32 @@
+(require 'f)
+
+(defvar google-translate-support-path
+  (f-dirname load-file-name))
+
+(defvar google-translate-features-path
+  (f-parent google-translate-support-path))
+
+(defvar google-translate-root-path
+  (f-parent google-translate-features-path))
+
+(add-to-list 'load-path google-translate-root-path)
+
+(require 'google-translate)
+(require 'espuds)
+(require 'ert)
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
