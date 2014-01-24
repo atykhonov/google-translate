0fd52bcd2a42ecd1e58834ff04a9899ae021e8a1
Andrey Tykhonov <atykhonov@gmail.com>
2014-01-24 20:03:23 +0200

    * google-translate-core-ui.el: Refactored google-translate-translate

--- google-translate-core-ui.el
+++ google-translate-core-ui.el
@@ -310,6 +310,53 @@ apply FACE to it."
     (facemenu-set-face face beg (point))
     (fill-region beg (point))))
 
+(defun google-translate--buffer-output-translation-title (source-language 
+                                                          target-language 
+                                                          auto-detected-language)
+  (insert (format "Translate from %s to %s:\n"
+                  (if (string-equal source-language "auto")
+                      (format "%s (detected)"
+                              (google-translate-language-display-name
+                               auto-detected-language))
+                    (google-translate-language-display-name
+                     source-language))
+                  (google-translate-language-display-name
+                   target-language))))
+
+(defun google-translate--buffer-output-translating-text (text)
+  (google-translate-paragraph
+   text
+   'google-translate-text-face))
+
+(defun google-translate--buffer-output-text-phonetic (text-phonetic)
+  (when (and google-translate-show-phonetic
+             (not (string-equal text-phonetic "")))
+    (google-translate-paragraph
+     text-phonetic
+     'google-translate-phonetic-face)))
+
+(defun google-translate--buffer-output-translation (translation)
+  (google-translate-paragraph
+   translation
+   'google-translate-translation-face))
+
+(defun google-translate--buffer-output-translation-phonetic (translation-phonetic)
+  (when (and google-translate-show-phonetic
+             (not (string-equal translation-phonetic "")))
+    (google-translate-paragraph
+     translation-phonetic
+     'google-translate-phonetic-face)))
+
+(defun google-translate--buffer-output-detailed-translation (detailed-translation translation)
+  (loop for item across detailed-translation do
+        (let ((index 0))
+          (unless (string-equal (aref item 0) "")
+            (insert (format "\n%s\n" (aref item 0)))
+            (loop for translation across (aref item 1) do
+                  (insert (format "%2d. %s\n"
+                                  (incf index) translation)))))))
+
+
 (defun google-translate-translate (source-language target-language text)
   "Translate TEXT from SOURCE-LANGUAGE to TARGET-LANGUAGE.
 
@@ -317,73 +364,30 @@ Pops up a buffer named *Google Translate* with available translations
 of TEXT.  To deal with multi-line regions, sequences of white space
 are replaced with a single space.  If the region contains not text, a
 message is printed."
-  (let ((text-stripped
-         (replace-regexp-in-string "[[:space:]\n\r]+" " " text)))
-    (if (or (= (length text-stripped) 0)
-            (string= text-stripped " "))
-        (message "Nothing to translate.")
-      (let* ((json
-              (json-read-from-string
-               (google-translate--insert-nulls
-                (google-translate--http-response-body
-                 (google-translate--format-request-url
-                  `(("client" . "t")
-                    ("ie"     . "UTF-8")
-                    ("oe"     . "UTF-8")
-                    ("sl"     . ,source-language)
-                    ("tl"     . ,target-language)
-                    ("text"   . ,text-stripped)))))))
-             (text-phonetic
-              (mapconcat #'(lambda (item) (aref item 3))
-                         (aref json 0) ""))
-             (translation
-              (mapconcat #'(lambda (item) (aref item 0))
-                         (aref json 0) ""))
-             (translation-phonetic
-              (mapconcat #'(lambda (item) (aref item 2))
-                         (aref json 0) ""))
-             (dict (aref json 1)))
-        (with-output-to-temp-buffer "*Google Translate*"
-          (set-buffer "*Google Translate*")
-          (insert
-           (format "Translate from %s to %s:\n"
-                   (if (string-equal source-language "auto")
-                       (format "%s (detected)"
-                               (google-translate-language-display-name
-                                (aref json 2)))
-                     (google-translate-language-display-name
-                      source-language))
-                   (google-translate-language-display-name
-                    target-language)))
-          (google-translate-paragraph
-           text
-           'google-translate-text-face)
-          (when (and google-translate-show-phonetic
-                     (not (string-equal text-phonetic "")))
-            (google-translate-paragraph
-             text-phonetic
-             'google-translate-phonetic-face))
-          (google-translate-paragraph
-           translation
-           'google-translate-translation-face)
-          (when (and google-translate-show-phonetic
-                     (not (string-equal translation-phonetic "")))
-            (google-translate-paragraph
-             translation-phonetic
-             'google-translate-phonetic-face))
-          (when dict
-            ;; DICT is, if non-nil, a dictionary article represented by
-            ;; a vector of items, where each item is a 2-element vector
-            ;; whose zeroth element is the name of a part of speech and
-            ;; whose first element is a vector of translations for that
-            ;; part of speech.
-            (loop for item across dict do
-                  (let ((index 0))
-                    (unless (string-equal (aref item 0) "")
-                      (insert (format "\n%s\n" (aref item 0)))
-                      (loop for translation across (aref item 1) do
-                            (insert (format "%2d. %s\n"
-                                            (incf index) translation))))))))))))
+      (let* ((buffer-name "*Google Translate*")
+             (json (google-translate-request source-language
+                                             target-language
+                                             text))
+             (auto-detected-language (aref json 2))
+             (text-phonetic (google-translate-json-text-phonetic json))
+             (translation (google-translate-json-translation json))
+             (translation-phonetic (google-translate-json-translation-phonetic json))
+             (detailed-translation (google-translate-json-detailed-translation json)))
+
+        (with-output-to-temp-buffer buffer-name
+          (set-buffer buffer-name)
+          (google-translate--buffer-output-translation-title source-language
+                                                             target-language
+                                                             auto-detected-language)
+          (google-translate--buffer-output-translating-text)
+          (google-translate--buffer-output-text-phonetic text-phonetic)
+          (google-translate--buffer-output-translation translation)
+          (google-translate--buffer-output-translation-phonetic translation-phonetic)
+          (when detailed-translation
+            (google-translate--buffer-output-detailed-translation
+             detailed-translation
+             translation)))))
+
 
 (provide 'google-translate-core-ui)
 
