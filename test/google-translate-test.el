8afba1fa273cc42e472a97ee34f14799c8400200
Andrey Tykhonov <atykhonov@gmail.com>
2014-01-24 18:54:06 +0200

    * test/fixtures/word/2.fixture: Tests added

--- test/google-translate-test.el
+++ test/google-translate-test.el
@@ -1,4 +1,78 @@
-(ert-deftest test-insert-nulls ()
+(ert-deftest test-google-translate-insert-nulls ()
   (should (string-equal
-           (google-translate-insert-nulls "[,[,[,,],,],,]")
+           (google-translate--insert-nulls "[,[,[,,],,],,]")
            "[null,[null,[null,null,null],null,null],null,null]")))
+
+(ert-deftest test-google-translate-request-words-fixtures ()
+  (dolist (file (f-files google-translate-test/word-fixture-path))
+    (let ((fixture (th-google-translate-load-fixture file)))
+      (th-google-translate-request-fixture fixture)
+      ;; assertions are skipped. In case of no errors assume that test pass.
+)))
+
+(ert-deftest test-google-translate--strip-string-with-spaces ()
+  (should (string-equal
+           (google-translate--strip-string "    spaces     spaces ")
+           " spaces spaces ")))
+
+(ert-deftest test-google-translate--strip-string-with-carriage-return-and-line-feeds ()
+  (should (string-equal
+           (google-translate--strip-string "\n\n\r\nspaces\r\n\n\r\r\n\nspaces\r\n\r\r\r\n")
+           " spaces spaces ")))
+
+(ert-deftest test-google-translate--trim-string-with-spaces ()
+  (should (string-equal
+           (google-translate--trim-string "    spaces   spaces     ")
+           "spaces   spaces")))
+
+(ert-deftest test-google-translate--trim-string-with-carriage-return-and-line-feeds ()
+  (should (string-equal
+           (google-translate--trim-string "\n\n\r\nspaces   spaces\n\n\n\r\n")
+           "spaces   spaces")))
+
+(ert-deftest test-google-translate-prepare-text-for-request ()
+  (should (string-equal
+           (google-translate-prepare-text-for-request "\n\r\nspaces\r   \n\n\rspaces\n\n\r")
+           "spaces spaces")))
+
+(ert-deftest test-google-translate-json-text-phonetic ()
+  (dolist (file (f-files google-translate-test/word-fixture-path))
+    (let ((fixture (th-google-translate-load-fixture file)))
+      (should (string-equal
+               (th-google-translate-fixture-text-phonetic fixture)
+               (google-translate-json-text-phonetic
+                (th-google-translate-request-fixture fixture)))))))
+
+(ert-deftest test-google-translate-json-translation-phonetic ()
+  (dolist (file (f-files google-translate-test/word-fixture-path))
+    (let ((fixture (th-google-translate-load-fixture file)))
+      (should (string-equal
+               (th-google-translate-fixture-translation-phonetic fixture)
+               (google-translate-json-translation-phonetic
+                (th-google-translate-request-fixture fixture)))))))
+
+(ert-deftest test-google-translate-json-translation ()
+  (dolist (file (f-files google-translate-test/word-fixture-path))
+    (let ((fixture (th-google-translate-load-fixture file)))
+      (should (string-equal
+               (th-google-translate-fixture-translation fixture)
+               (google-translate-json-translation
+                (th-google-translate-request-fixture fixture)))))))
+
+(ert-deftest test-google-translate-json-detailed-translation ()
+  (dolist (file (f-files google-translate-test/word-fixture-path))
+    (let* ((fixture (th-google-translate-load-fixture file))
+           (index 0)
+           (detailed-translation (google-translate-json-detailed-translation
+                                  (th-google-translate-request-fixture fixture)))
+           (fixture-dt (th-google-translate-fixture-detailed-translation fixture)))
+      (when fixture-dt
+        (loop for item across detailed-translation do
+              (let ((index 0))
+                (unless (string-equal (aref item 0) "")
+                  (should (string-equal (aref fixture-dt index)
+                                        (aref item 0)))
+                  (loop for translation across (aref item 1) do
+                        (incf index)
+                        (should (string-equal (aref fixture-dt index)
+                                              (aref item 1)))))))))))
--- test/test-helper.el
+++ test/test-helper.el
@@ -6,6 +6,18 @@
 (defvar google-translate-test/test-path
   (f-dirname load-file-name))
 
+(defvar google-translate-test/fixture-path
+  (f-expand "fixtures" google-translate-test/test-path))
+
+(defvar google-translate-test/word-fixture-path
+  (f-expand "word" google-translate-test/fixture-path))
+
+(defvar google-translate-test/phrase-fixture-path
+  (f-expand "phrase" google-translate-test/fixture-path))
+
+(defvar google-translate-test/sentence-fixture-path
+  (f-expand "sentence" google-translate-test/fixture-path))
+
 (defvar google-translate-test/root-path
   (f-parent google-translate-test/test-path))
 
@@ -17,3 +29,71 @@
 (require 'google-translate
          (f-expand "google-translate"
                    google-translate-test/root-path))
+
+(defun th-google-translate-load-fixture (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (set-buffer-multibyte t)
+    (goto-char (point-min))
+    (re-search-forward (format "\n\n\n"))
+    (delete-region (point-min) (point))
+    (buffer-string)))
+
+(defun th-google-translate-fixture-response (fixture)
+  (with-temp-buffer
+    (insert fixture)
+    (set-buffer-multibyte t)
+    (goto-char (point-min))
+    (let ((stpoint (point)))
+      (re-search-forward (format "\n\n"))
+      (re-search-forward (format "\n\n"))
+      (buffer-substring-no-properties stpoint (point)))))
+
+(defun th-google-translate-temp-buffer (contents)
+  (let ((buffer (get-buffer-create "*Google Translate Temp Buffer*")))
+    (with-current-buffer buffer
+      (erase-buffer)
+      (insert contents)
+      buffer)))
+
+(defun th-google-translate-request-fixture (fixture)
+  (with-mock
+   (stub url-retrieve-synchronously =>
+         (th-google-translate-temp-buffer
+          (th-google-translate-fixture-response fixture)))
+   (google-translate-request "en" "ru" "return")))
+
+(defun th-google-translate-fixture-text-phonetic (fixture)
+  (with-current-buffer (th-google-translate-temp-buffer fixture)
+    (goto-char (point-min))
+    (search-forward "text-phonetic:")
+    (forward-line 1)
+    (buffer-substring-no-properties (line-beginning-position)
+                                    (line-end-position))))
+
+(defun th-google-translate-fixture-translation-phonetic (fixture)
+  (with-current-buffer (th-google-translate-temp-buffer fixture)
+    (goto-char (point-min))
+    (search-forward "translation-phonetic:")
+    (forward-line 1)
+    (buffer-substring-no-properties (line-beginning-position)
+                                    (line-end-position))))
+
+(defun th-google-translate-fixture-translation (fixture)
+  (with-current-buffer (th-google-translate-temp-buffer fixture)
+    (goto-char (point-min))
+    (search-forward "translation:")
+    (forward-line 1)
+    (buffer-substring-no-properties (line-beginning-position)
+                                    (line-end-position))))
+
+(defun th-google-translate-fixture-detailed-translation (fixture)
+  (with-current-buffer (th-google-translate-temp-buffer fixture)
+    (goto-char (point-min))
+    (search-forward "detailed-translation:")
+    (let ((line " ") (result '()))
+      (while (not (equal line ""))
+        (forward-line 1)
+        (setq line (buffer-substring-no-properties (line-beginning-position)
+                                                   (line-end-position)))
+        (setq result (append result '(,line)))))))
