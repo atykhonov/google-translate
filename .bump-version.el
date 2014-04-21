06aa2980162f32bd36ff78b126113d146738c56d
Andrey Tykhonov <atykhonov@gmail.com>
2014-04-21 16:49:43 +0300

    Add .bump-version.el.

---
+++ .bump-version.el
@@ -0,0 +1,8 @@
+((:files
+  ("google-translate.el"
+   "google-translate-core.el"
+   "google-translate-core-ui.el"
+   "google-translate-default-ui.el"
+   "google-translate-pkg.el"
+   "google-translate-smooth-ui.el"))
+ (:current-version "0.8.2"))
