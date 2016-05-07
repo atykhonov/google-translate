5aa8b31ac4186fd88b269cd9d1f45611564d0853
Andrey Tykhonov <atykhonov@gmail.com>
2016-05-07 15:52:06 +0300

    Bump version 0.11.5 => 0.11.6

--- google-translate-core-ui.el
+++ google-translate-core-ui.el
@@ -653,7 +653,7 @@ http://www.gnu.org/software/emacs/manual/html_node/elisp/The-Echo-Area.html)"
 (defun google-translate-help-buffer-output-translation (gtos)
   "Output translation to the help buffer."
   (and google-translate-pop-up-buffer-set-focus
-      (select-window (display-buffer "*Help*")))
+       (select-window (display-buffer "*Help*")))
   (google-translate-buffer-insert-translation gtos))
 
 (defun google-translate-buffer-insert-translation (gtos)
