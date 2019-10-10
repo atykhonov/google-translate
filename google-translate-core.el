b40b826cc186b2ce32f6e265699b88c55d90a210
lee <986374081@qq.com>
2019-10-10 14:58:01 +0800

    Update google-translate-core.el

--- google-translate-core.el
+++ google-translate-core.el
@@ -74,10 +74,10 @@
   :group 'processes)
 
 (defvar google-translate-base-url
-  "http://translate.google.com/translate_a/single")
+  "http://translate.google.cn/translate_a/single")
 
 (defvar google-translate-listen-url
-  "http://translate.google.com/translate_tts")
+  "http://translate.google.cn/translate_tts")
 
 (defun google-translate--format-query-string (query-params)
   "Format QUERY-PARAMS as a query string.
