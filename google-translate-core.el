723b7e0dc7c1eae7e1ad5a312bd071676f63d2ca
stardiviner <numbchild@gmail.com>
2020-07-04 12:02:23 +0800

    fix CI error seq-empty-p depend on emacs version "25.1"

--- google-translate-core.el
+++ google-translate-core.el
@@ -5,6 +5,7 @@
 ;; Author: Oleksandr Manzyuk <manzyuk@gmail.com>
 ;; Maintainer: Andrey Tykhonov <atykhonov@gmail.com>
 ;; URL: https://github.com/atykhonov/google-translate
+;; Package-Requires: ((emacs "25.1"))
 ;; Version: 0.12.0
 ;; Keywords: convenience
 
